1e61503a7b3b68ac6a1d9cae5600f809ab1ffc3c
RohitPr <bladekiller95@gmail.com>
2021-02-27 17:09:19 +0530

    Initial

--- 35.Flight_Deal_Finder/flight_search.py
+++ 35.Flight_Deal_Finder/flight_search.py
@@ -1,12 +1,3 @@
-from datetime import datetime
-import requests
-
 class FlightSearch:
-
     #This class is responsible for talking to the Flight Search API.
-    
-    # API KEY Declaration
-    KIWI_API = "https://tequila-api.kiwi.com/v2/search" 
-    KIWI_API_KEY = "M_SjAkzFGq67IT8j-30dmjlGs5-SFnB5"
-
-    response = requests.get(KIWI_API)
+    pass

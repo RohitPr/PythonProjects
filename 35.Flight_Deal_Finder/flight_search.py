c8d5944cb5c818153efd058cacb586ebfce7ec15
RohitPr <bladekiller95@gmail.com>
2021-02-27 17:34:03 +0530

    Initial

--- 35.Flight_Deal_Finder/flight_search.py
+++ 35.Flight_Deal_Finder/flight_search.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+import requests
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    # This class is responsible for talking to the Flight Search API.
+    KIWI_API = "https://tequila-api.kiwi.com/v2/search?fly_from=LGA&fly_to=MIA&dateFrom=01/04/2021&dateTo=02/04/2021"

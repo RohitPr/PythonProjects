a6f1080e060fb1b372cbe91fe3419dda4da368d1
RohitPr <bladekiller95@gmail.com>
2021-02-28 20:30:56 +0530

    Final update

--- 36.Top_100_Movies_BS4/hacker_news_parse.py
+++ 36.Top_100_Movies_BS4/hacker_news_parse.py
@@ -1,23 +1,25 @@
-from typing import Text
 from bs4 import BeautifulSoup
 import requests
 
+# Parsing Hacker News site to get the HTML Data
+
 response = requests.get("https://news.ycombinator.com/")
 yc_page = response.text
-
 soup = BeautifulSoup(yc_page, "html.parser")
 
-article_data = []
 article_votes = []
 article_texts = []
 article_links = []
-article_votes = []
+
+# Parsing HTML with Soup to get Article Links and Title
 
 article_data = soup.findAll(name="a", class_="storylink")
 article_vote_data = [
     int(score.getText().split()[0])
     for score in soup.findAll(name="span", class_="score")
 ]
+
+# Storing Data in List and checking the article with Highest Votes
 
 for data in article_data:
     text = data.getText()
@@ -28,3 +30,4 @@ for data in article_data:
 max_vote = max(article_vote_data)
 print(article_texts[article_vote_data.index(max_vote)])
 print(article_links[article_vote_data.index(max_vote)])
+print(max_vote)

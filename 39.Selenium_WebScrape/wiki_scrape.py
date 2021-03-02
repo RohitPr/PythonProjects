7ace1feeae49cca3a719b829cd17b06f8ce87451
RohitPr <bladekiller95@gmail.com>
2021-03-03 02:16:51 +0530

    Final Update

--- 39.Selenium_WebScrape/wiki_scrape.py
+++ 39.Selenium_WebScrape/wiki_scrape.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 chrome_driver = "D:\Study\Chrome Driver\chromedriver.exe"
 
@@ -6,6 +7,11 @@ driver = webdriver.Chrome(executable_path=chrome_driver)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
 count = driver.find_element_by_xpath('//*[@id="articlecount"]/a[1]')
-print(count.text)
+# count.click()
 
-driver.quit()
+portal_link = driver.find_element_by_link_text("All portals")
+# portal_link.click()
+
+search = driver.find_element_by_name("search")
+search.send_keys("Python")
+search.send_keys(Keys.ENTER)

7ed9bdc10c1f4323c2641362f575956adc721a0a
RohitPr <bladekiller95@gmail.com>
2021-03-04 20:52:13 +0530

    Final

--- 41.LinkedIn_Automation/main.py
+++ 41.LinkedIn_Automation/main.py
@@ -1,29 +1,73 @@
+import time
+
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 
-linkedIn_url = "https://www.linkedin.com/jobs/search/?f_AL=true&geoId=109710172&keywords=python%20developer&location" \
-               "=Bengaluru%2C%20Karnataka%2C%20India "
+ACCOUNT_EMAIL = "Email"
+ACCOUNT_PASSWORD = "Password"
+PHONE = "Phone"
 
-chrome_driver = "D:\Study\Chrome Driver\chromedriver.exe"
-driver = webdriver.Chrome(executable_path=chrome_driver)
+chrome_driver_path = "D:\Study\Chrome Driver\chromedriver.exe"
+driver = webdriver.Chrome(chrome_driver_path)
+driver.get(
+    "https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&keywords=marketing%20intern&location=London%2C"
+    "%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0")
 
-# Signs into Linked In using my Credentials
+time.sleep(2)
+sign_in_button = driver.find_element_by_link_text("Sign in")
+sign_in_button.click()
 
-driver.get(linkedIn_url)
-sign_in_btn = driver.find_element_by_class_name("nav__button-secondary")
-sign_in_btn.click()
+time.sleep(5)
+email_field = driver.find_element_by_id("username")
+email_field.send_keys(ACCOUNT_EMAIL)
+password_field = driver.find_element_by_id("password")
+password_field.send_keys(ACCOUNT_PASSWORD)
+password_field.send_keys(Keys.ENTER)
 
-email_input = driver.find_element_by_name("session_key")
-email_input.send_keys("email")
-password = driver.find_element_by_name("session_password")
-password.send_keys("password")
-password.send_keys(Keys.ENTER)
+time.sleep(5)
 
-# Clicks on the first Job and Saves the Job Details to the profile(Did not Apply as this is my Main account)
+all_listings = driver.find_elements_by_css_selector(".job-card-container--clickable")
 
-job = driver.find_element_by_css_selector(".artdeco-entity-lockup__title a")
-job.click()
-save = driver.find_element_by_css_selector(".jobs-save-button span")
-save.click()
+for listing in all_listings:
+    print("called")
+    listing.click()
+    time.sleep(2)
 
+    # Try to locate the apply button, if can't locate then skip the job.
+    try:
+        apply_button = driver.find_element_by_css_selector(".jobs-s-apply button")
+        apply_button.click()
+        time.sleep(5)
+
+        # If phone field is empty, then fill your phone number.
+        phone = driver.find_element_by_class_name("fb-single-line-text__input")
+        if phone.text == "":
+            phone.send_keys(PHONE)
+
+        submit_button = driver.find_element_by_css_selector("footer button")
+
+        # If the submit_button is a "Next" button, then this is a multi-step application, so skip.
+        if submit_button.get_attribute("data-control-name") == "continue_unify":
+            close_button = driver.find_element_by_class_name("artdeco-modal__dismiss")
+            close_button.click()
+            time.sleep(2)
+            discard_button = driver.find_elements_by_class_name("artdeco-modal__confirm-dialog-btn")[1]
+            discard_button.click()
+            print("Complex application, skipped.")
+            continue
+        else:
+            submit_button.click()
+
+        # Once application completed, close the pop-up window.
+        time.sleep(2)
+        close_button = driver.find_element_by_class_name("artdeco-modal__dismiss")
+        close_button.click()
+
+    # If already applied to job or job is no longer accepting applications, then skip.
+    except NoSuchElementException:
+        print("No application button, skipped.")
+        continue
+
+time.sleep(5)
 driver.quit()

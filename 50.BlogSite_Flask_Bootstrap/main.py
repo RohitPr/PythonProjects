951721a8cd376933c947dc3235af5009e5af6944
RohitPr <bladekiller95@gmail.com>
2021-03-13 15:26:03 +0530

    Homepage update

--- 50.BlogSite_Flask_Bootstrap/main.py
+++ 50.BlogSite_Flask_Bootstrap/main.py
@@ -1,11 +1,28 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+import requests
 
+API_URL = "https://api.npoint.io/2722c84e8ff4b2ddbd15"
+
+posts = requests.get(API_URL).json()
 
 app = Flask(__name__)
 
+
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)

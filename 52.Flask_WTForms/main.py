9cb75de9cccdced71e9b60235711df8735434b92
RohitPr <bladekiller95@gmail.com>
2021-03-13 21:44:19 +0530

    Final

--- 52.Flask_WTForms/main.py
+++ 52.Flask_WTForms/main.py
@@ -1,31 +1,30 @@
 from flask import Flask, render_template
-from flask.globals import request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Length
 from flask_bootstrap import Bootstrap
 
 
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[
+                             DataRequired(), Length(min=8)])
+    submit = SubmitField(label="Log In")
+
+
 app = Flask(__name__)
-Bootstrap(app)
 app.secret_key = "secret_key"
-
-
-class MyForm(FlaskForm):
-    email = StringField(label='email', validators=[
-                        DataRequired(), Email()])
-    password = PasswordField(label='password', validators=[DataRequired()])
-    submit = SubmitField(label='submit')
+Bootstrap(app)
 
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    login_form = MyForm()
+    login_form = LoginForm()
     if login_form.validate_on_submit():
         if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
             return render_template("success.html")

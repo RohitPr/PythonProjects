fa3f451dc6278e8443aa21799d79cac69cbd17a4
RohitPr <bladekiller95@gmail.com>
2021-03-17 21:40:40 +0530

    Final

--- 54.SQL_Lite_BookShelf/main.py
+++ 54.SQL_Lite_BookShelf/main.py
@@ -1,20 +1,103 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.fields.core import FloatField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+Bootstrap(app)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 
-all_books = []
+# Created a New Database using SQLite and added a table named Books
+# db = sqlite3.connect("books-collection.db")
+# cursor = db.cursor()
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), unique=False, nullable=False)
+    rating = db.Column(db.Float, unique=False, nullable=False)
+
+    # This allows each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+db.create_all()
+
+if db.session.query(Book).count() < 1:
+    new_book = Book(title="Harry Potter",
+                    author="J. K. Rowling", rating=9.3)
+    db.session.add(new_book)
+    db.session.commit()
+
+
+class BookForm(FlaskForm):
+    book_name = StringField('Book Name', validators=[DataRequired()])
+    author = StringField("Author",
+                         validators=[DataRequired()])
+    rating = FloatField("Rating", validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+# all_books = []
 
 
 @app.route('/')
 def home():
-    pass
+    all_books = db.session.query(Book).all()
+    return render_template("index.html", books=all_books)
 
 
-@app.route("/add")
+@app.route('/add', methods=['GET', 'POST'])
 def add():
-    pass
+    form = BookForm()
+    if form.validate_on_submit():
+        new_book = Book(
+            title=form.book_name.data,
+            author=form.author.data,
+            rating=form.rating.data,
+        )
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('add.html', form=form)
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        # UPDATE RECORD
+        book_id = request.form["id"]
+        book_to_update = Book.query.get(book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = Book.query.get(book_id)
+    return render_template("edit_rating.html", book=book_selected)
+
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+
+    # DELETE A RECORD BY ID
+    book_to_delete = Book.query.get(book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-

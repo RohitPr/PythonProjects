78154c8cd97321e6993c0b7fe7da314ed5d2b2ac
RohitPr <bladekiller95@gmail.com>
2021-03-23 22:57:44 +0530

    Update

--- 57.REST_API_RohitPr_blog/main.py
+++ 57.REST_API_RohitPr_blog/main.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
+from datetime import datetime
+import smtplib
 from flask_ckeditor import CKEditor, CKEditorField
 
+MY_EMAIL = "email"
+MY_PASSWORD = "password"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
+app.config['CKEDITOR_PKG_TYPE'] = 'full'
 Bootstrap(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
@@ -32,7 +37,7 @@ class CreatePostForm(FlaskForm):
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     author = StringField("Your Name", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = StringField("Blog Content", validators=[DataRequired()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
 
@@ -51,7 +56,50 @@ def show_post(post_id):
 @app.route("/new-post", methods=['GET', 'POST'])
 def new_post():
     form = CreatePostForm()
+    if form.validate_on_submit():
+        new_blog = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            author=form.author.data,
+            img_url=form.img_url.data,
+            body=form.body.data,
+            date=datetime.now().strftime("%d %b, %Y")
+        )
+        db.session.add(new_blog)
+        db.session.commit()
+        flash('New Blog Added')
+        return redirect(url_for('index'))
     return render_template("new-post.html", form=form)
+
+
+@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = edit_form.author.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        flash('Blog Updated')
+        return redirect(url_for("show_post", post_id=post.id))
+    return render_template("make-edit.html", form=edit_form)
+
+
+@app.route("/delete/<int:post_id>")
+def delete_post(post_id):
+    post_to_delete = BlogPost.query.get(post_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 
 @app.route("/about")
@@ -59,9 +107,26 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template("contact.html")
+    if request.method == 'POST':
+        data = {
+            "name": request.form["name"],
+            "email": request.form["email"],
+            "message": request.form["message"],
+            "phone": request.form["phone"]
+        }
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=MY_EMAIL,
+                msg=f"Subject:You have a new Message!\n\nNAME : {data['name']}\n EMAIL : {data['email']} \n PHONE : {data['phone']} \n MESSAGE : {data['message']} "
+            )
+        return redirect(url_for('index'))
+    else:
+        return render_template('contact.html')
 
 
 if __name__ == "__main__":

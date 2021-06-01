8c67d57f772ba5018e8c482bd4a159beca05acbd
RohitPr <bladekiller95@gmail.com>
2021-06-01 16:26:16 +0530

    Initial

---
+++ 66.Spotify_ETL/spotify_etl.py
@@ -0,0 +1,12 @@
+import json
+import datetime
+from datetime import datetime
+import sqlite3
+import sqlite3
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+import requests
+
+DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
+USER_ID = "v5gqmoefaafptlgwxtbqasude"
+TOKEN = "BQDzpiPCNeemYgUA2szdb1q2SvZsvF0gdOEreAaWU1eopXSMvSgCBsg_SS70y-r4SIPPTOqoB4W5G9LjjzwZQMejMHKKL9o14CRF57U3-v1QNi3q0SRE6YmNrcwFZRMe3hvVat3POR7kDAUvW_IY2oVeMEVI8MoRP6fQ"

bf692bdc4f0cc693d89ec4564daa86311d797716
bRohitPr <bladekiller95@gmail.com>
2021-06-06 23:23:20 +0530

    update

---
+++ 66.Spotify_ETL/dags/spotify_dag.py
@@ -0,0 +1,28 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.timezone import datetime
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2021, 6, 6),
+    'email': 'airflow@example.com',
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG('spotify_dag', default_args=default_args,
+          description='Our Spotify DAG', schedule_interval=timedelta(days=1))
+
+
+def run_spotify_func():
+    print("Hello")
+
+
+run_etl = PythonOperator(task_id="complete_spotify_etl",
+                         python_callable=run_spotify_func, dag=dag)
+
+run_etl

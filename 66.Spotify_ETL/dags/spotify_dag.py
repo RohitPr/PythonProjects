52ba89da959a39646cc319abd0133f8bae312ffb
bRohitPr <bladekiller95@gmail.com>
2021-06-07 03:42:01 +0530

    Update

--- 66.Spotify_ETL/dags/spotify_dag.py
+++ 66.Spotify_ETL/dags/spotify_dag.py
@@ -1,24 +1,10 @@
-from datetime import timedelta
+from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.timezone import datetime
 from spotify_etl import run_spotify_etl
 
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2021, 6, 6),
-    'email': 'airflow@example.com',
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=1)
-}
+with DAG('spotify_dag', start_date=datetime(2021, 1, 1), schedule_interval="@daily", catchup=False) as dag:
 
-dag = DAG('spotify_dag', default_args=default_args,
-          description='Our Spotify DAG', schedule_interval=timedelta(days=1))
-
-run_etl = PythonOperator(task_id="complete_spotify_etl",
-                         python_callable=run_spotify_etl, dag=dag)
-
-run_etl
+    run_etl = PythonOperator(task_id="complete_spotify_etl",
+                             python_callable=run_spotify_etl)
--- 66.Spotify_ETL/dags/spotify_etl.py
+++ 66.Spotify_ETL/dags/spotify_etl.py
@@ -49,73 +49,72 @@ def run_spotify_etl():
 
 #  Take Yesterday's Date and change it to miliseconds and pass it as header for the request
 
+    today = datetime.now()
+    yesterday = today - timedelta(days=1)
+    yesterday_timestamp = int(yesterday.timestamp()) * 1000
 
-today = datetime.now()
-yesterday = today - timedelta(days=1)
-yesterday_timestamp = int(yesterday.timestamp()) * 1000
+    spotify_request = requests.get(
+        "https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_timestamp), headers=headers_data)
 
-spotify_request = requests.get(
-    "https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_timestamp), headers=headers_data)
+    spotify_data = spotify_request.json()
+    print(spotify_data)
 
-spotify_data = spotify_request.json()
-print(spotify_data)
+    song_name = []
+    artist_name = []
+    played_at = []
+    timestamp = []
 
-song_name = []
-artist_name = []
-played_at = []
-timestamp = []
+    # Extracting Required data
 
-# Extracting Required data
+    for song in spotify_data["items"]:
+        song_name.append(song["track"]["name"])
+        artist_name.append(song["track"]["artists"][0]["name"])
+        played_at.append(song["played_at"])
+        timestamp.append(song["played_at"][:10])
 
-for song in spotify_data["items"]:
-    song_name.append(song["track"]["name"])
-    artist_name.append(song["track"]["artists"][0]["name"])
-    played_at.append(song["played_at"])
-    timestamp.append(song["played_at"][:10])
+    # Preparing Dict for DF object
 
-# Preparing Dict for DF object
+    songs_dict = {
+        "song_name": song_name,
+        "artist_name": artist_name,
+        "played_at": played_at,
+        "timestamp": timestamp
+    }
 
-songs_dict = {
-    "song_name": song_name,
-    "artist_name": artist_name,
-    "played_at": played_at,
-    "timestamp": timestamp
-}
+    songs_df = pd.DataFrame(songs_dict, columns=[
+                            "song_name", "artist_name", "played_at", "timestamp"])
 
-songs_df = pd.DataFrame(songs_dict, columns=[
-                        "song_name", "artist_name", "played_at", "timestamp"])
+    # Validating Data for abnormalities
 
-# Validating Data for abnormalities
+    if check_valid_data(songs_df):
+        print("Data Valid, proceed to loading data")
 
-if check_valid_data(songs_df):
-    print("Data Valid, proceed to loading data")
+    #  Creating DB Engine and connecting to the DB
 
+    db_engine = sqlalchemy.create_engine(database_location)
+    connection = sqlite3.connect('my_spotify_songs.sqlite')
+    cursor = connection.cursor()
 
-#  Creating DB Engine and connecting to the DB
+    sql_query = """
+        CREATE TABLE IF NOT EXISTS my_spotify_songs(
+            song_name VARCHAR(200),
+            artist_name VARCHAR(200),
+            played_at VARCHAR(200),
+            timestamp VARCHAR(200),
+            CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+        )
+    """
 
-db_engine = sqlalchemy.create_engine(database_location)
-connection = sqlite3.connect('my_spotify_songs.sqlite')
-cursor = connection.cursor()
+    cursor.execute(sql_query)
+    print("SQL DB Created")
 
-sql_query = """
-    CREATE TABLE IF NOT EXISTS my_spotify_songs(
-        song_name VARCHAR(200),
-        artist_name VARCHAR(200),
-        played_at VARCHAR(200),
-        timestamp VARCHAR(200),
-        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
-    )
-"""
+    # Appending the data to the Created DB
 
-cursor.execute(sql_query)
-print("SQL DB Created")
+    try:
+        songs_df.to_sql("my_spotify_songs", engine,
+                        index=False, if_exists=append)
+    except:
+        print("Data already exists in the Database")
 
-# Appending the data to the Created DB
-
-try:
-    songs_df.to_sql("my_spotify_songs", engine, index=False, if_exists=append)
-except:
-    print("Data already exists in the Database")
-
-connection.close()
-print("Connection Closed")
+    connection.close()
+    print("Connection Closed")
